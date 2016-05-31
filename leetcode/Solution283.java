f91ce8f2f8b0f4fcdd6eba8ba518e88dd1e7aa97
Apologise <603061018@qq.com>
2016-05-31 13:55:54 +0800

    Move zeros O(n)

--- leetcode/Solution283.java
+++ leetcode/Solution283.java
@@ -1,28 +1,44 @@
 import java.util.*;
 
 public class Solution283{
+
+//O(n^2)
 	public static int[] MoveZeros(int[] nums){
-		for(int i = 0; i < nums.length&&nums[i]==0; i++){
-			System.out.println("===");
-			System.out.println(nums[i]);
-			System.out.println("===");
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i]==0){
 			for(int j = i+1; j < nums.length; j++){
-				System.out.println("---");
-				System.out.println(nums[j]);
-				System.out.println("---");
 				if(nums[j]!=0){
 				nums[i] = nums[j];
 				nums[j] = 0;
-
 				break;
 			}
+			}
+		}
+	}
+		return nums;
+	}
+
+
+//O(n)
+	public static int[] MoveZeros1(int[] nums){
+		int index = -1;
+		for(int i = 0; i < nums.length; i++){
+			//记录0第一次出现的位置
+			if(index == -1&&nums[i]==0){
+				index = i;
+			}
+			if(nums[i]!=0&&index!=-1){
+				nums[index] = nums[i];
+				nums[i] = 0;
+				index++;
 			}
 		}
 		return nums;
 	}
 
 	public static void main(String[] args){
-		int[] a ={0,1,0,3,4};
+		int[] a ={1,0,0,3};
 		System.out.println(Arrays.toString(Solution283.MoveZeros(a)));
+		System.out.println(Arrays.toString(Solution283.MoveZeros1(a)));
 	}
 }
