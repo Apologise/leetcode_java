06f83d49475581071881ea36f77ad8567af1e17c
Apologise <603061018@qq.com>
2016-06-28 10:09:57 +0800

    PowerOfTwo

---
+++ leetcode/Solution231.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+public class Solution231 {
+	public boolean isPowerOfTwo(int n){
+		return n>0&&(n&(n-1))==0;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
