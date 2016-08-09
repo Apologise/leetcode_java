59770b1f796f230bae6edb4dabc4c8dbe22eb027
Apologise <603061018@qq.com>
2016-08-09 10:54:00 +0800

    mergeTwoSortLists

---
+++ leetcode/Solution21_3.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+public class Solution21_3 {
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if( l1 == null) { return l2;}
+        if( l2 == null) { return l1;}
+        
+        ListNode mergeHead;
+        if( l1.val < l2.val){
+        	
+        }
+    }
+}
